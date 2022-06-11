9d3a3bc8d656e5d37126dbdde5d7d96b62d216af
Amirhossein Sarajan <amirhossein.sarajan@seb.se>
2022-06-11 20:50:44 +0800

    moves escapeString to separate file

---
+++ escapeString.js
@@ -0,0 +1,33 @@
+function escapeString(riddle) {
+    const letters = Array.from('abcdefghijklmnopqrstuvwxyz');
+    const unavailLetters = new Set();
+
+    let qmarkIndex = riddle.indexOf('?');
+    while (qmarkIndex >= 0) {
+        part1 = riddle.substring(0, qmarkIndex);
+        part2 = riddle.substring(qmarkIndex + 1);
+
+        if (part1.length > 0)
+            unavailLetters.add(part1[part1.length - 1]);
+        if (part2.length > 0)
+            unavailLetters.add(part2[0]);
+
+        let availLetter = findFirstAvailableLetter();       
+
+        riddle = part1 + availLetter + part2;
+
+        unavailLetters.clear();
+        qmarkIndex = riddle.indexOf('?');
+    }
+    return riddle;
+
+    function findFirstAvailableLetter() {
+        let availLetterIndex = 0;
+        while (availLetterIndex < letters.length &&
+            unavailLetters.has(letters[availLetterIndex]))
+            availLetterIndex++;
+        return  letters[availLetterIndex];
+    }
+}
+
+module.exports = escapeString
--- riddle.test.js
+++ riddle.test.js
@@ -1,54 +1,24 @@
-function solution(riddle) {
-    const letters = Array.from('abcdefghijklmnopqrstuvwxyz');
-    const unavailLetters = new Set();
-
-    let qmarkIndex = riddle.indexOf('?');
-    while (qmarkIndex >= 0) {
-        part1 = riddle.substring(0, qmarkIndex);
-        part2 = riddle.substring(qmarkIndex + 1);
-
-        if (part1.length > 0)
-            unavailLetters.add(part1[part1.length - 1]);
-        if (part2.length > 0)
-            unavailLetters.add(part2[0]);
-
-        let availLetter = findFirstAvailableLetter();       
-
-        riddle = part1 + availLetter + part2;
-
-        unavailLetters.clear();
-        qmarkIndex = riddle.indexOf('?');
-    }
-    return riddle;
-
-    function findFirstAvailableLetter() {
-        let availLetterIndex = 0;
-        while (availLetterIndex < letters.length &&
-            unavailLetters.has(letters[availLetterIndex]))
-            availLetterIndex++;
-        return  letters[availLetterIndex];
-    }
-}
+const escapeString = require("./escapeString");
 
 test("returns riddle when no qmark", () => {
-    expect(solution("abcd")).toBe("abcd");
+    expect(escapeString("abcd")).toBe("abcd");
 })
 test("replace the single '?' with 'a'", () => {
-    expect(solution("?")).toBe("a");
+    expect(escapeString("?")).toBe("a");
 })
 test("replace just one qmark with next non-adjacent charachetr", () => {
-    expect(solution("b?")).toBe("ba");
-    expect(solution("?b")).toBe("ab");
-    expect(solution("a?b")).toBe("acb");
-    expect(solution("c?d")).toBe("cad");
-    expect(solution("b?f")).toBe("baf");
+    expect(escapeString("b?")).toBe("ba");
+    expect(escapeString("?b")).toBe("ab");
+    expect(escapeString("a?b")).toBe("acb");
+    expect(escapeString("c?d")).toBe("cad");
+    expect(escapeString("b?f")).toBe("baf");
 })
 test("replace two distant qmark with non-adjacent charachetr", () => {
-    expect(solution("a?bc?b")).toBe("acbcab");
-    expect(solution("c?d")).toBe("cad");
-    expect(solution("b?f")).toBe("baf");
+    expect(escapeString("a?bc?b")).toBe("acbcab");
+    expect(escapeString("c?d")).toBe("cad");
+    expect(escapeString("b?f")).toBe("baf");
 })
 
 test("replaces ?? with ab", () => {
-    expect(solution("??")).toBe("ab");
+    expect(escapeString("??")).toBe("ab");
 })
